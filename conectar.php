0e338314e58358350c5e5d5c8353e014edaa0a2f
multipleo <pedroh_s_@hotmail.com>
2015-06-26 23:07:04 -0300

    Revert "teste"
    This reverts commit 0307470bd7f3c5db2707fe13de3e19959bef31bc.

--- conectar.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$pass = "vertrigo";
-$user = "root";
-$banco = "wishlist";
-$con = mysql_connect($server, $user, $pass) or die("Erro ao conectar com o banco");
-mysql_select_db($banco) or die("Não foi possivel encontrar a database: $banco");
-?>
